b9dd5481649bcbd0582ff941a183a7b681d9a94f
Jorge <jandresruiz1993@gmail.com>
2026-01-25 22:25:40 +1300

    Add csrf call.

--- app/_lib/auth.ts
+++ app/_lib/auth.ts
@@ -2,8 +2,17 @@ export const getUser = async () => {
     //
 };
 
+export const getCsrfCookie = async () => {
+    await fetch(process.env.NEXT_PUBLIC_CSRF_API_BASE_URL || "http://localhost/sanctum/csrf-cookie", {
+        method: "GET",
+        credentials: "include",
+    });
+};
+
 export const login = async (email: string, password: string, remember: boolean) => {
     try {
+        await getCsrfCookie();
+
         const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost/api/tasks/"}login`, {
             method: "POST",
             headers: {
