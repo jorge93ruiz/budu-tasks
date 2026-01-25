e8a671416a3e57d9be8a71d6f7cabb630f7293e4
Jorge <jandresruiz1993@gmail.com>
2026-01-25 23:06:53 +1300

    Correct padding.

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -10,7 +10,7 @@ export default async function MainLayout({
     return (
         <div className="grid grid-rows-[auto_1fr_auto] min-h-dvh font-(family-name:--font-geist-sans)">
             <Header className="container mx-auto" />
-            <main className="py-4 px-4 sm:px-8 container mx-auto">{children}</main>
+            <main className="py-8 px-4 sm:px-8 container mx-auto">{children}</main>
             <Footer className="row-start-3 container mx-auto" />
         </div>
     );
