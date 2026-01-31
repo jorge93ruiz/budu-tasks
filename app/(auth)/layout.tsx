dec128dc06a5c7aa2660b4e8bfc3177be169e9f7
Jorge <jandresruiz1993@gmail.com>
2026-01-31 22:11:41 +1300

    Correct login.

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -8,7 +8,13 @@ export default async function AuthLayout({
 }>) {
     return (
         <div className="grid grid-rows-[1fr_auto] items-center justify-items-center min-h-dvh font-(family-name:--font-geist-sans) bg-background">
-            <main className="py-4 px-8">{children}</main>
+            <main className="py-4 px-8 flex flex-col gap-y-8 items-center">
+                <div className="px-8 sm:px-12 py-2">
+                    <div className="text-[50px] leading-0 text-muted">budu</div>
+                    <div className="font-bold tracking-wider text-[150px] leading-37.5">tasks</div>
+                </div>
+                {children}
+            </main>
             <Footer />
         </div>
     );
